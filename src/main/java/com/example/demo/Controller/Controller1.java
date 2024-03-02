3432336be66d16dfca8840db9cb869f1b4abfb37
nthraj1989 <niitrajnish@gmail.com>
2024-03-02 21:22:58 +0530

    commit "removed extra space"

--- src/main/java/com/example/demo/Controller/Controller1.java
+++ src/main/java/com/example/demo/Controller/Controller1.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class Controller1 {
+public class Controller1{
 
     @GetMapping("/greeting/{name}")
     public String displayName(@PathVariable String name){
